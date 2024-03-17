c5a5823c31fb62cabc89a8df20f9b381653fd947
“talatkuyuk” <talatkuyuk@gmail.com>
2024-03-18 01:03:42 +0300

    added test coverage

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { visit, type Visitor, type VisitorResult } from "unist-util-visit";
 import type { Plugin, Transformer } from "unified";
-import type { Paragraph, Root, Text } from "mdast";
+import type { Paragraph, Parent, Root, Text } from "mdast";
 import { u } from "unist-builder";
 
 export const REGEX = /\+\+\s*([^+]*[^ ])?\s*\+\+/;
@@ -18,7 +18,7 @@ export const REGEX_GLOBAL = /\+\+\s*([^+]*[^ ])?\s*\+\+/g;
 export const plugin: Plugin<void[], Root> = () => {
   /**
    *
-   * constracts the custom <ins> node
+   * constructs a custom <ins> node
    *
    */
   const constructInsNode = (insertedText: string | undefined): Paragraph => {
@@ -40,7 +40,7 @@ export const plugin: Plugin<void[], Root> = () => {
    * visits the text nodes which match with the ins syntax (++insertedtext++)
    *
    */
-  const visitor: Visitor<Text> = function (node, index, parent): VisitorResult {
+  const visitor: Visitor<Text, Parent> = function (node, index, parent): VisitorResult {
     /* istanbul ignore next */
     if (!parent || typeof index === "undefined") return;
 
@@ -57,8 +57,9 @@ export const plugin: Plugin<void[], Root> = () => {
     for (let index = 0; index < matches.length; index++) {
       const match = matches[index];
 
-      const mIndex = match.index ?? 0;
-      const mLength = match[0].length; // match[0] is the matched input
+      const [matched, insertedText] = match;
+      const mIndex = match.index!;
+      const mLength = matched.length;
 
       // could be a text part before each matched part
       const textPartIndex = index === 0 ? 0 : prevMatchIndex + prevMatchLength;
@@ -73,9 +74,6 @@ export const plugin: Plugin<void[], Root> = () => {
         const textNode = u("text", textValue) as Text;
         children.push(textNode);
       }
-
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const [input, insertedText] = match;
 
       const inserterNode = constructInsNode(insertedText);
 
